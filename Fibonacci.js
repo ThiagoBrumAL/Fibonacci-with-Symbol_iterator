92106d115bc23bb85d670871750792507823430d
Thiago_de_Almeida_Brum <thiagobrum222@gmail.com>
2025-06-01 00:23:26 -0300

    Second commit

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,8 +1,8 @@
-function fib(v){
+function fib(new_input_value){
         const Fibonacci = {
         [Symbol.iterator]: function*(){
-            c = 0;
-            n = 1;
+            let c = 0;
+            let n = 1;
 
             while(true){
                 yield c;
@@ -13,11 +13,20 @@ function fib(v){
 
     const itr = Fibonacci[Symbol.iterator]();
 
-    for(let i=0; i <= v; i++){
-        console.log(itr.next().value);
-        
+
+    const h1 = document.getElementById("fib-number");
+
+    for(let i=0; i <= new_input_value; i++){
+        value = itr.next().value;
+        if(i === new_input_value){
+            h1.innerHTML = value;
+        }
     }
 
 }
 
-fib(8);
+document.getElementById("button").addEventListener("click", ()=> {
+    const input_value = document.getElementById("field-value").value;
+    let new_input_value = parseInt(input_value);
+    fib(new_input_value);
+})
